edc222676b5371a239c5db091a247823102b8ba4
Kishore <90689873+Kishore015@users.noreply.github.com>
2024-11-30 17:50:21 +0530

    JS programming basics
    Created basic programs using JS

---
+++ dmas.js
@@ -0,0 +1,21 @@
+function divide(a,b){
+    return a/b;
+}
+function multiply(a,b){
+    return a-b;
+}
+function add(a,b){
+    return a+b;
+}
+function substract(a,b){
+    return a-b;
+}
+function reminder(a,b){
+    return a%b;
+}
+
+console.log("Divide",divide(5,10))
+console.log("Multiply",multiply(5,10))
+console.log("Add",add(5,10))
+console.log("Subtract",substract(5,10))
+console.log("Subtract",reminder(5,10))
