114af3d8744b4d80220f47ea87ce208ff4c87610
Kishore <90689873+Kishore015@users.noreply.github.com>
2024-12-07 22:58:17 +0530

    Added 5 new problems

--- problems.js
+++ problems.js
@@ -111,6 +111,107 @@ function swapValues(a,b){
     b = a ^ b
     a = a ^ b
 
+// kms to miles
+// 1 mile = km * 0.621371;
+const km =3;
+const miles = km * 0.621371;
+console.log(`${km} kms is equal to ${miles} miles`)
+
+// miles to kms
+// 1 km = miles/0.621371;
+const mile = 10;
+const kms = mile/0.621371;
+console.log(`${mile} miles is equal to ${kms} kms`)
+
+// fahrenheit = celsius * (9/5) + 32
+// C = F-32 * (5/9)
+const celsius = 42;
+const fahrenheit = (celsius * 9/5) + 32;
+console.log(fahrenheit)
+
+// Generate a Random Number
+const randomNum  = Math.random();
+console.log(randomNum)
+
+// Math.random() * (highestNumber - lowestNumber) + lowestNumber
+
+const genNum = (Math.random()*(999-101)+101)*1000;
+const round = Math.floor(genNum)
+console.log(`genNum is ${genNum} and rounded Num is ${round}`);
+
+const enteredNum = Math.floor((Math.random()*(999-101)+101)*1000)
+
+if(round === enteredNum){
+    console.log("Same OTP, provide access to user")
+}else{
+    console.log("different otp, throw error")
+}
+
+console.log(`${enteredNum}`);
+let arr = [];
+let set = new Set();
+for(let i=0;i<=100;i++){
+    const uniqueNum = Math.floor((Math.random()*(999-101)+101)*1000);
+    if(set.has(uniqueNum)){
+        console.log("duplicate found")
+    }else{
+        set.add(uniqueNum)
+        console.log("Unique OTP Generated")
+    }
+    arr = [...set]
+}
+console.log(`${arr}`, typeof arr)
+console.log(Array.isArray(arr)); // true
+
+// program that checks if the number is positive, negative or zero
+// input from the user
+const number = parseInt(prompt("Enter a number: "));
+
+// check if number is greater than 0
+if (number > 0) {
+    console.log("The number is positive");
+}
+
+// check if number is 0
+else if (number == 0) {
+  console.log("The number is zero");
+}
+
+// if number is less than 0
+else {
+     console.log("The number is negative");
+}
+
+// check if the number is positive, negative or zero
+const num = prompt("Enter a number: ");
+
+if (num >= 0) {
+    if (num == 0) {
+        console.log("The number is zero");
+    } else {
+        console.log("The number is positive");
+    }
+} else {
+    console.log("The number is negative");
+}
+
+// program to check if the number is even or odd
+// take input from the user
+const numx = prompt("Enter a number: ");
+
+//check if the number is even
+if(numx % 2 == 0) {
+    console.log("The number is even.");
+}
+
+// if the number is odd
+else {
+    console.log("The number is odd.");
+}
+
+// ternary operator
+const result = (numx % 2  == 0) ? "even" : "odd";
+
     console.log(`The value of a after swapping: ${a}`);
     console.log(`The value of b after swapping: ${b}`);
 }
