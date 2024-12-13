a0d0b15e200f030ce6930bbf488faeefcda83026
Kishore <90689873+Kishore015@users.noreply.github.com>
2024-12-13 20:51:24 +0530

    Added Factors of a Positive Number Program

--- problems.js
+++ problems.js
@@ -476,3 +476,19 @@ while (true) {
     }
     min++;
 }
+
+// program to find the factors of an integer
+
+// user input
+const num = prompt('Enter a positive number: ');
+
+console.log(`The factors of ${num} is:`);
+
+// looping through 1 to num
+for(let i = 1; i <= num; i++) {
+
+    // check if number is a factor
+    if(num % i == 0) {
+        console.log(i);
+    }
+}
